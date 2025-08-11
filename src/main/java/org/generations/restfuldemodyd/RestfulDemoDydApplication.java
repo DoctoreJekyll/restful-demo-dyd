554c491570b16fdc41571d6621312515d807a408
Joseajierro <jrmar0805@gmail.com>
2025-08-11 10:18:43 +0200

    Initial commit

---
+++ src/main/java/org/generations/restfuldemodyd/RestfulDemoDydApplication.java
@@ -0,0 +1,13 @@
+package org.generations.restfuldemodyd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulDemoDydApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfulDemoDydApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/generations/restfuldemodyd/RestfulDemoDydApplicationTests.java
@@ -0,0 +1,13 @@
+package org.generations.restfuldemodyd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulDemoDydApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
