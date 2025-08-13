a99a1fd6d25de3ee88a805427a52c81fb9c7b2b5
Joseajierro <jrmar0805@gmail.com>
2025-08-13 13:09:27 +0200

    Models, repo (suer and roles) and jwt dependency

---
+++ src/main/java/org/generations/restfuldemodyd/model/Role.java
@@ -0,0 +1,20 @@
+package org.generations.restfuldemodyd.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+}
---
+++ src/main/java/org/generations/restfuldemodyd/model/User.java
@@ -0,0 +1,28 @@
+package org.generations.restfuldemodyd.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.Set;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String username;
+    private String password;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "user_roles",
+            joinColumns = @JoinColumn(name="user_id"),
+            inverseJoinColumns = @JoinColumn(name="role_id")
+    )
+    private Set<Role> roles;
+}
